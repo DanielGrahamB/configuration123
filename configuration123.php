4c2d3877986b5e241340189955ecb08f96d4d2f1
Boaz Daniel Graham <daniel@prositeweb.ca>
2026-08-16 21:57:57 -0400

    chore: update GitHub plugin header URI parameter

--- configuration123.php
+++ configuration123.php
@@ -8,7 +8,7 @@
  * Requires PHP:      8.1
  * Author:            Boaz Daniel Graham
  * Author URI:        https://boazdanielgraham.ca/projects/configuration-123/
- * GitHub Plugin URI: https://github.com/DanielGrahamB/configuration123
+ * GitHub URI:        https://github.com/DanielGrahamB/configuration123
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       configuration123
